363b1bfdf8b0483180f2eaa724d7fe6810ef0081
rifkifikri <rifkifikri000@gmail.com>
2024-07-13 14:56:40 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Contact View</h1>
+    <div>
+        <table border="3">
+            <th>
+                <td>Identitas</td>
+            </th>
+            <tbody>
+                <tr>
+                    <td> Nama : </td>
+                    <td> M. Rifki Fikri Firdaus</td>
+                </tr>
+                <tr>
+                    <td> Email : </td>
+                    <td> Rifki@gmail.com</td>
+                </tr>
+                <tr>
+                    <td> Social Media : </td>
+                    <td> <a href="">rifkifikri_</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/blog', function(){
+    return view('blog');
+});
+Route::get('/contact', function (){
+    return view('contact');
+});
